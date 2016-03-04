73ff9eaead342825654f4ee8f75bdac499d216ec
joaquincasanova <joaquin.casanova@gmail.com>
2016-03-04 14:34:31 +0000

    comments

--- code/py/cvusda_example.py
+++ code/py/cvusda_example.py
@@ -2,11 +2,14 @@ import agcv
 import pantilt
 import mlx90614
 
-#position=pantilt.PanTilt()
-#position.drone()
+position=pantilt.PanTilt()
+#Move camera with arrow keys - "q" to quit
+position.drone()
 test = agcv.CV()
+#Otsu segmentation
 test.OtsuHV()
+#EM segmentation
 test.emHV()
-
+#read the Melexis IR sensor
 tmp = mlx90614.MLX()
 print tmp.readObjTempK()
